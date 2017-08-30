f1a8a9abf530a4a0aba1565df0212b9fc56faa82
CuZzN187 <boogens04@aol.com>
2017-08-29 22:49:59 -0600

    changes to VS
    adding linkedLists

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -32,9 +32,34 @@ namespace ConsoleApplication1
 
             // Pass by ref
             int numberTest = 500;
+            //calling update function with params
             update(ref numberTest);
             Console.WriteLine("New value: " + numberTest);
-            Console.ReadKey();
+            Console.WriteLine();
+            //calling linkedList function with no params
+            linkedList();
+            Console.ReadKey(); //<<<<<needed to pause the console so that pressing ENTER will close out console
+        }
+
+        public static void linkedList()
+        {
+            //.NET Libraries have alot of short cut functionality like linked lists, hashtables, etc...
+
+            //this uses the .NET framework libraries to make a Linked List
+            LinkedList<string> lista = new LinkedList<string>();
+
+            //adding to your linked list
+            lista.AddLast("Sabree");
+            lista.AddFirst("Juan");
+            lista.AddBefore(lista.First, "Robb");
+            lista.AddAfter(lista.Last, "Bradley");
+            lista.AddBefore(lista.First, "The 404");
+
+            //simple foreach loop
+            foreach (var val in lista)
+            {
+                Console.WriteLine(val);
+            }
         }
     }
 }
