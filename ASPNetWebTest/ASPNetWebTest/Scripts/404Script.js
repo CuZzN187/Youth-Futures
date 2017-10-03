0053e8b9f3a905e37ac98499e5e2e389872e587b
Echostt <Echohoward05@gmail.com>
2017-10-02 21:22:00 -0600

    Toggle does not actually toggle now

--- ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
+++ ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
@@ -1,13 +1,5 @@
 ﻿function toggleView() {
-    var viewBox = document.getElementById("attributeViewBox");
-    var toggleView = document.getElementById("toggleView");
-    if (viewBox.style.width == "0px") {
-        viewBox.style.width = "300px";
-        toggleView.style.right = "300px";
-        console.log("start 0");
-    } else if (viewBox.style.width == "300px") {
-        viewBox.style.width = "0px";
-        toggleView.style.right = "0px";
-        console.log("start 300");
-    }
+    document.getElementById("attributeViewBox").style.width = "0px";
+    document.getElementById("toggleView").style.right = "0px";
+
 }
