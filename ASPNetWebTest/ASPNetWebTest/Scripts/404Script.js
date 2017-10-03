550872551cee9aab8bf64929272907a010df8699
Echostt <Echohoward05@gmail.com>
2017-10-02 21:41:42 -0600

    Hide the button

--- ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
+++ ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
@@ -1,5 +1,5 @@
 ﻿function toggleView() {
     document.getElementById("attributeViewBox").style.width = "0px";
     document.getElementById("toggleView").style.right = "0px";
-
+    document.getElementById("btnSubmit").style.visibility = "hidden";
 }
