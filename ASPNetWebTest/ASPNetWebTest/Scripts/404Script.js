4105139f37960156e10b327a4f21c7b7610b504e
Echostt <Echohoward05@gmail.com>
2017-10-02 22:31:13 -0600

    Some stubs for adding event handlers

--- ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
+++ ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
@@ -3,3 +3,20 @@
     document.getElementById("toggleView").style.right = "0px";
     document.getElementById("btnSubmit").style.visibility = "hidden";
 }
+
+function loadElementsToEditView() {
+    var nameBox = document.getElementById("elementName");
+    nameBox.innerHTML = this.innerHTML; //incorrect targetting of right-side assignment
+}
+
+//loops through img and p tags in the page and adds an event handler to load for edit view
+function eventLoader() {
+    var p = document.getElementsByName("p");
+    for (var i = 0; i < p.length; ++i) {
+        p[i].addEventListener("click", loadElementsToEditView());
+    }
+    var img = document.getElementsByTagName("img");
+    for (var i = 0; i < img.length; ++i) {
+        img[i].addEventListener("click", loadElementsToEditView());
+    }
+}
