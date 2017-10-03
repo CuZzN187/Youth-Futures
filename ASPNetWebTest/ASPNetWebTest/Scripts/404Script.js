a439061c802f5e7a7c20f7b63480d852c4a53e47
Echostt <Echohoward05@gmail.com>
2017-10-02 23:32:45 -0600

    Presentation Changes
    Added some scripts for loading in info to attribute view for presentation purposes.

--- ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
+++ ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
@@ -1,7 +1,16 @@
 ﻿function toggleView() {
-    document.getElementById("attributeViewBox").style.width = "0px";
-    document.getElementById("toggleView").style.right = "0px";
-    document.getElementById("btnSubmit").style.visibility = "hidden";
+    var attrViewBox = document.getElementById("attributeViewBox");
+    var viewToggle = document.getElementById("toggleView");
+    var btnSub = document.getElementById("btnSubmit");
+    if (attrViewBox.style.visibility == "visible") {
+        attrViewBox.style.visibility = "hidden";
+        viewToggle.style.right = "0px";
+        btnSub.style.visibility = "hidden";
+    } else if (attrViewBox.style.visibility == "hidden") {
+        attrViewBox.style.visibility = "visible";
+        viewToggle.style.right = "300px";
+        btnSub.style.visibility = "visible";
+    }
 }
 
 function loadElementsToEditView() {
@@ -19,4 +28,25 @@ function eventLoader() {
     for (var i = 0; i < img.length; ++i) {
         img[i].addEventListener("click", loadElementsToEditView());
     }
+}
+
+function loadBigHi() {
+    document.getElementById("elementName").children[1].innerHTML = "HI";
+    document.getElementById("elementAttr1").children[1].innerHTML = "quicksand-bold";
+    document.getElementById("elementAttr2").children[1].innerHTML = "rgb(255, 255, 255)";
+    document.getElementById("elementAttr3").children[1].innerHTML = "214px";
+}
+
+function loadBeds() {
+    document.getElementById("elementName").children[1].innerHTML = "14 WARM BEDS. YOUTH 12-17. YOUR TEMPORARY HOME";
+    document.getElementById("elementAttr1").children[1].innerHTML = "quicksand-bold";
+    document.getElementById("elementAttr2").children[1].innerHTML = "rgb(71, 71, 71)";
+    document.getElementById("elementAttr3").children[1].innerHTML = "38px";
+}
+
+function loadContact() {
+    document.getElementById("elementName").children[1].innerHTML = "Have questions? Send us a text!";
+    document.getElementById("elementAttr1").children[1].innerHTML = "montserrat-semibold";
+    document.getElementById("elementAttr2").children[1].innerHTML = "rgb(255, 255, 255)";
+    document.getElementById("elementAttr3").children[1].innerHTML = "36px";
 }
