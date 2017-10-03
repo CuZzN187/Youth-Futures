3ee102a6221c10b9cea472af2226a4355dbf162c
Echostt <Echohoward05@gmail.com>
2017-10-02 21:11:37 -0600

    Toggle actually toggles now

--- ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
+++ ASPNetWebTest/ASPNetWebTest/Scripts/404Script.js
@@ -1,5 +1,13 @@
 ﻿function toggleView() {
-    document.getElementById("attributeViewBox").style.width = "0px";
-    document.getElementById("toggleView").style.right = "0px";
-    console.log("RAN");
+    var viewBox = document.getElementById("attributeViewBox");
+    var toggleView = document.getElementById("toggleView");
+    if (viewBox.style.width == "0px") {
+        viewBox.style.width = "300px";
+        toggleView.style.right = "300px";
+        console.log("start 0");
+    } else if (viewBox.style.width == "300px") {
+        viewBox.style.width = "0px";
+        toggleView.style.right = "0px";
+        console.log("start 300");
+    }
 }
