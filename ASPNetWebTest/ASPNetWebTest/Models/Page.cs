d14e7431293372cc74940326b4392573e2f66c30
Echostt <Echohoward05@gmail.com>
2017-11-02 09:51:24 -0600

    Added some starting data annotations

--- ASPNetWebTest/ASPNetWebTest/Models/Page.cs
+++ ASPNetWebTest/ASPNetWebTest/Models/Page.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASPNetWebTest.Models {
     [Table(name: "Page")]
     public class Page {
+        [Key]
+        [Required]
         public int PageID { get; set; }
-        public string PageLink { get; set; }
-        public string PageName { get; set; }
-        public string PageHtml { get; set; }
-    }
 
-    public class PageContext : DbContext {
-        public DbSet<Page> Pages { get; set; }
+        [Required]
+        public string PageLink { get; set; }
+
+        [Required]
+        public string PageName { get; set; }
+
+        [Required]
+        public string PageHtml { get; set; }
     }
 }
